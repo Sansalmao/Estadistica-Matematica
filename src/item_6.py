2de23eab32b84ee10f8f00a7652cdecf13f5c022
Sans <167203085+Sansalmao@users.noreply.github.com>
2024-12-05 17:54:00 -0400

    item_6.py
    Funciona

--- src/item_6.py
+++ src/item_6.py
@@ -1,28 +1,29 @@
 import pandas as pd
 import statsmodels.formula.api as sm
+from tabulate import tabulate
 
-data = pd.read_csv("./src/datos_.csv")
+# Cargar los datos
+datos = pd.read_csv('datos_.csv')
 
-data = pd.get_dummies(data, columns=["sex", "smoker", "region"], drop_first=True)
+# Convertir variables categóricas a numéricas usando one-hot encoding
+datos = pd.get_dummies(datos, columns=['sex', 'smoker', 'region'], drop_first=True)
 
-model = sm.ols(
-    "expenses ~ age + sex_male + bmi + children + smoker_yes + region_northwest + region_southeast + region_southwest",
-    data=data,
-).fit()
+# Crear el modelo lineal
+modelo = sm.ols('expenses ~ age + sex_male + bmi + children + smoker_yes + region_northwest + region_southeast + region_southwest', data=datos).fit()
 
-print(model.summary())
+# Nivel de significancia
+nivel_significancia = 0.05
 
-significance_level = 0.05
+# Obtener la tabla de resultados del modelo
+resultados = modelo.summary2().tables[1] 
 
-for coefficient in model.pvalues.index:
-    p_valor = model.pvalues[coefficient]
-    if p_valor < significance_level:
-        print(f"\nSe rechaza H0 para el coeficiente de '{coefficient}'.")
-        print(
-            f"Hay evidencia de que '{coefficient}' tiene un efecto significativo en 'expenses'."
-        )
-    else:
-        print(f"\nNo se rechaza H0 para el coeficiente de '{coefficient}'.")
-        print(
-            f"No hay suficiente evidencia para afirmar que '{coefficient}' tiene un efecto significativo en 'expenses'."
-        )
+# Personalizar la columna "Rechazar H0"
+resultados['Rechazar H0'] = resultados.apply(lambda fila: f"Se rechaza H0 porque p ({fila['P>|t|']:.3f}) < {nivel_significancia}" if fila['P>|t|'] < nivel_significancia else f"No se rechaza H0 porque p ({fila['P>|t|']:.3f}) >= {nivel_significancia}", axis=1)
+
+# Formatear los resultados como una lista ordenada
+headers = [" ", "Coef.", "Std.Err.", "t", "P>|t|", "[0.025", "0.975]", "Rechazar H0"]  
+tabla_formateada = tabulate(resultados, headers=headers, tablefmt="plain")  
+
+# Imprimir los resultados con el título "Resultados"
+print("\nResultados:\n")  
+print(tabla_formateada)

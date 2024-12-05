2c6f8d89cddcd0ee437ef4c94b46c4d01bc03a3e
Dante Dev <jdap.dmc@gmail.com>
2024-12-04 23:08:18 -0400

    [FEAT]: items 7,8 y 9

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-window = tk.Tk()
-
-window.title("Estadistica Matematica Tercer Parcial")
-
-window.geometry("1080x920")
-
-window.mainloop()
---
+++ src/smooker_bmi.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
+
+data = pd.read_csv("./src/lib/datos_.csv")
+le = LabelEncoder()
+
+data["sex_aux"] = le.fit_transform(data["sex"])
+data["smoker_aux"] = le.fit_transform(data["smoker"])
+data["region_aux"] = le.fit_transform(data["region"])
+data["smoker_bmi"] = data["smoker_aux"] * data["bmi"]
+
+X = data[
+    ["age", "sex_aux", "bmi", "children", "smoker_aux", "region_aux", "smoker_bmi"]
+]
+y = data["expenses"]
+
+model = LinearRegression()
+
+model.fit(X, y)
+
+r_squared = model.score(X, y)
+print("Coeficiente de determinación con interacción:", r_squared)
